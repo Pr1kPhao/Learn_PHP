8bbdd701b21d4942978964e9e4aeb3874df7cb72
PrikPhao <137915799+Pr1kPhao@users.noreply.github.com>
2023-06-27 22:58:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learn_PHP
+ Just chilling with PHP bruv.
---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>php dog shit</title>
+</head>
+<body>
+    <?php
+ $cars = array("BMW", "Honda", "Lambo", "Benz");
+echo "My Favorite car " . $cars[0] . " , " $cars[1];
+?>
+</body>
+</html>
