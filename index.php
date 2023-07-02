380f064bd1ac7d0179aac26b37ff6070929b8ef1
PrikPhao <137915799+Pr1kPhao@users.noreply.github.com>
2023-07-02 19:54:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,66 @@
     <title>php dog shit</title>
 </head>
 <body>
+
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+    Name : <input type="text" name="fname">
+    <input type="submit">
+</form>
     <?php
- $cars = array("BMW", "Honda", "Lambo", "Benz");
-echo "My Favorite car " . $cars[0];
-echo count($cars);
+//  1. Indexed Arrays
+//  2. Assciative Arrays
+//  3. Multidimensional Arrays
+    $cars = array("BMW" => "1m", "Ford" => "2m", "Honda" => "3m");
+    
+    foreach($cars as $name => $value)
+
+    // $cars = ["BMW"] = "1m";
+    // $cars = ["Ford"] = "2m";
+    // $cars = ["Honda"] = "3m";
+// PHP Super global variables
+    /**
+     * $GLOBALS
+     * $_SERVER
+     * $_REQUEST
+     * $_POST
+     * $_GET
+     * $_FILES
+     * $_ENV
+     * $_COOKIE
+     * $_SESSION
+     */
+
+
+  /**$x = 10;
+     $y = 5;
+
+     function addition (){
+        $GLOBALS ['z'] = $GLOBALS ['x'] + $GLOBALS ['y'];
+     }
+
+     addition();
+     echo $z;*/
+
+/**echo $_SERVER ['PHP_SELF'];
+echo "<br>";
+echo $_SERVER ['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER ['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER ['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER ['SCRIPT_NAME'];
+echo "<br>";*/
+
+if ($_SERVER ["REQYEST_METHOD"] == "POST") {
+    $name = $_REQUEST ['fname'];
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
+
 ?>
 </body>
 </html>
